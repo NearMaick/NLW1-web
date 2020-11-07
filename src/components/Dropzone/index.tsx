391d6ac99af46bb974d55142a819f82b92c45e84
Maick Souza <maick_a_s@msn.com>
2020-11-07 13:17:42 -0300

    feat(web): Upload image #1

--- src/components/Dropzone/index.tsx
+++ src/components/Dropzone/index.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
+import { FiUpload } from 'react-icons/fi';
 
 import './styles.css';
 
@@ -11,12 +12,10 @@ const Dropzone: React.FC = () => {
 
   return (
     <div className="dropzone" {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Imagem do estabelecimento</p>
-      )}
+      <p>
+        <FiUpload />
+        Imagem do estabelecimento
+      </p>
     </div>
   );
 };
