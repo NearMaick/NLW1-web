977cd1fab52db05df767acc3fb3d884b641da6ec
Maick Souza <maick_a_s@msn.com>
2020-11-07 13:26:06 -0300

    feat(web): Upload image #2

--- src/components/Dropzone/index.tsx
+++ src/components/Dropzone/index.tsx
@@ -1,21 +1,36 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { FiUpload } from 'react-icons/fi';
 
 import './styles.css';
 
 const Dropzone: React.FC = () => {
+  const [selectedFileUrl, setSelectedFileUrl] = useState('');
+
   const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles);
+    const file = acceptedFiles[0];
+
+    const fileUrl = URL.createObjectURL(file);
+
+    setSelectedFileUrl(fileUrl);
   }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: 'image/*',
+  });
 
   return (
     <div className="dropzone" {...getRootProps()}>
-      <p>
-        <FiUpload />
-        Imagem do estabelecimento
-      </p>
+      <input {...getInputProps()} accept="image/*" />
+
+      {selectedFileUrl ? (
+        <img src={selectedFileUrl} alt="Point thumbnail" />
+      ) : (
+        <p>
+          <FiUpload />
+          Imagem do estabelecimento
+        </p>
+      )}
     </div>
   );
 };
